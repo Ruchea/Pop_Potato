b0892090f4139218d860903a5eaea239b40c5773
Vince014 <vince014@cougars.csusm.edu>
2016-05-29 09:46:01 -0700

    Everything
    All the files are in there now, sorry.

---
+++ Assets/SCRIPTS/Potato_StartController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Potato_StartController : MonoBehaviour {
+
+	private Rigidbody2D start_PotatoBody;
+	public float potatoBounceForce = 500f;
+
+	// Use this for initialization
+	void Start () {
+		start_PotatoBody = GetComponent<Rigidbody2D> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonUp ("Jump")) {
+			start_PotatoBody.AddForce (transform.up * potatoBounceForce);
+		}
+	}
+
+	//Function called to start the game
+	public void StartGame()
+	{
+		Application.LoadLevel ("GameScene");
+	}
+}
